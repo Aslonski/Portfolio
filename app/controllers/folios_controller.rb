1727628c53861f2c82d2c43bbcc61f14aff0ad32
Aslonski <slonski.andrey@gmail.com>
2019-01-12 22:14:28 -0600

    add new and create actions

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -2,4 +2,22 @@ class FoliosController < ApplicationController
   def index
     @folio_items = Folio.all
   end
+
+  def new
+    @folio_item = Folio.new
+  end
+
+  def create
+    @folio_item = Folio.new(params.require(:folio).permit(:title, :body))
+
+
+    respond_to do |format|
+      if @folio_item.save
+        format.html { redirect_to @folio_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
