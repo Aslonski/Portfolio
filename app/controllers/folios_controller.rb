27c82121b510fc8f29433f7a0fa8cbc1aa5cf1b4
Aslonski <slonski.andrey@gmail.com>
2019-01-16 21:36:10 -0600

    Add edit functionality

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -13,9 +13,24 @@ class FoliosController < ApplicationController
 
     respond_to do |format|
       if @folio_item.save
-        format.html { redirect_to @folio_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @folios_path, notice: 'Blog was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+  def edit
+    @folio_item = Folio.find(params[:id])
+  end
+
+  def update
+        @folio_item = Folio.find(params[:id])
+
+    respond_to do |format|
+      if @folio_item.update(params.require(:folio).permit(:title, :body))
+        format.html { redirect_to @folios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
